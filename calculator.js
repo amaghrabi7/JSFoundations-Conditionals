8c8bdedd28a955988a0de1767168ec5cb6535d1c
Mano <apple@Macintosh.local>
2021-02-02 18:52:11 +0300

    Fixed it

--- calculator.js
+++ calculator.js
@@ -4,6 +4,10 @@ let Operation = "Multiplication";
 
 if (Operation == "Multiplication") {
     console.log(FirstNumber * SecondNumber);
-}
-
-debugger
+} else if (Operation =="Addition"){
+    console.log(FirstNumber + SecondNumber);
+ }else if (Operation =="Subtraction"){
+    console.log(FirstNumber - SecondNumber);
+ }else if (Operation =="Division"){
+    console.log(FirstNumber / SecondNumber);
+ }
