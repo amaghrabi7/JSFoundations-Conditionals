203567763fbaa16b252d0131f2af8189dbb8deac
Mano <apple@Macintosh.local>
2021-02-02 18:33:44 +0300

    Conditionals

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
-// Define the numbers and the operation here
+let FirstNumber = 7;  
+let SecondNumber = 10;
+let Operation = "Multiplication";
 
-// Write if-statements here
+if (Operation == "Multiplication") {
+    console.log(FirstNumber * SecondNumber);
+}
+
